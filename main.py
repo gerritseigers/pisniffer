be718392100729402b5d43693825e94aa390e63c
Gerrit Seigers <gerrrit.seigers@wur.nl>
2024-11-02 16:07:36 +0000

    Implement threading for sensor data reading and Flask app; add restart logic for threads

--- main.py
+++ main.py
@@ -25,9 +25,23 @@ app.register_blueprint(controllers)
 def run_flask_app():
     app.run(host='0.0.0.0', port=5000)
 
+def start_measurement_thread():
+    global data_thread
+
+    data_thread = threading.Thread(target=read_sensors, args=(message_queue,))
+    data_thread.daemon = True 
+    data_thread.start()
+
+def start_flask_thread():
+    global flask_thread
+
+    flask_thread = threading.Thread(target=run_flask_app)
+    flask_thread.daemon = True 
+    flask_thread.start()    
 
 async def sendToIotHub(data):
     try:
+
         # Create an instance of the IoT Hub Client class
         device_client = IoTHubDeviceClient.create_from_connection_string(
             connectionString
@@ -48,6 +62,10 @@ async def sendToIotHub(data):
 
 
 def main():
+
+    start_measurement_thread()
+    start_flask_thread()
+
     # Run an infinite while loop to send data every 5 seconds
     logger.info("Starting the data generation loop...")
 
@@ -58,20 +76,21 @@ def main():
 
     device = session.query(Device).filter_by(name="device1").first()
 
-    data_thread = threading.Thread(target=read_sensors, args=(message_queue,))
-    data_thread.daemon = True 
-    data_thread.start()
-
-    # Create and start the thread for the Flask app
-    flask_thread = threading.Thread(target=run_flask_app)
-    flask_thread.daemon = True 
-    flask_thread.start()
-
     while True:
         if not message_queue.empty():
+            logger.warning("Sending data to IoT Hub")
             while not message_queue.empty():
                 data = message_queue.get()
                 asyncio.run(sendToIotHub(data=json.dumps(data)))
+
+        if not data_thread.is_alive():
+            logger.error("Data thread has stopped. Restarting...")
+            start_measurement_thread()
+
+        if not flask_thread.is_alive():
+            logger.error("Flask thread has stopped. Restarting...")
+            start_flask_thread()
+                    
         device = session.query(Device).filter_by(name="device1").first()
         time.sleep(device.send_interval)
 
