00983b2f84e0751dfd6207d99767a4543d4f3d4f
gerrit seigers <gerrit@seigers.nl>
2024-11-05 12:16:58 +0100

    Add pulse counting functionality and update requirements for GPIO support

--- measurement/measurement.py
+++ measurement/measurement.py
@@ -7,10 +7,24 @@ import time
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
+import RPi.GPIO as GPIO
 from adafruit_ads1x15.analog_in import AnalogIn
 from shared import measurement_counter, increment_measurement_counter
 
+
+pulse_pin=18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pulse_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+pulse_counter = 0
+
+def count_pulse(channel):
+    global pulse_counter
+    pulse_counter+=1
+
 def read_sensors(message_queue):
+    global pulse_counter
 
     logger.info("Starting the data generation loop...")
     device = session.query(Device).first()
@@ -18,6 +32,8 @@ def read_sensors(message_queue):
 
     i2c = busio.I2C(board.SCL, board.SDA)
     ads = ADS.ADS1115(i2c)
+
+    GPIO.add_event_detect(pulse_pin, GPIO.RISING, callback=count_pulse)
 
     while True:
         try:
@@ -27,7 +43,7 @@ def read_sensors(message_queue):
             current_time = datetime.datetime.now()
             chan = AnalogIn(ads, ADS.P0)
             logger.info(f"Channel 0: {chan.voltage}V")
-
+            logger.info(f"Pulse counter: {pulse_counter}")
             measurement = Measurement(
                 value=chan.voltage, 
                 date=current_time, 
